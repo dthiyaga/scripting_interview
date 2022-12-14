032d1df1620aa00284f11d215998e7b1c1e49369
Dinesh Thiyagarajan <dthiyagaraja@dthiyagaraJMD6R.vmware.com>
2022-12-14 16:37:07 -0600

    Inital commit

--- simple_file_parser/parse_log.py
+++ simple_file_parser/parse_log.py
@@ -7,20 +7,25 @@ d=defaultdict(lambda:0,d)
 data={}
 fdate=lines[0].split(',')[0]
 curr_date=fdate
-print("fdate=",fdate)
 
 for line in lines:
     curr_date=line.split(',')[0]
     code=line.split()[2].split(',')[0]
     if(fdate!=curr_date):
-        print("Fdate is not curr date",fdate,curr_date)
         data[fdate]=d
-        print(data)
+
         fdate=curr_date
+        d={}
         d = defaultdict(lambda: 0, d)
     else:
         d[code]=int(d[code])+1
 if(fdate==curr_date):
     data[fdate] = d
-    
-print("final data =",data)
+
+
+
+for key, Value in data.items():
+    print(key)
+    for v,r in Value.items():
+        print(v,"=",r)
+#print("final data =",data)
