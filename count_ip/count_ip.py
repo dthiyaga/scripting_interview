7aeab867c89de150d21e2c36c5a081a747a884a5
Dinesh Thiyagarajan <dthiyagaraja@dthiyagaraJMD6R.vmware.com>
2022-12-14 16:43:44 -0600

    Inital commit

---
+++ count_ip/count_ip.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+f=open('ip_address.txt')
+count ={}
+count = defaultdict(lambda : 0,count)
+lines=f.readlines()
+for line in lines:
+    if(not line.startswith("#")):
+        ip=line.split()[0]
+        print(ip)
+        count[ip]=count[ip]+1
+
+print("Final =", count)
